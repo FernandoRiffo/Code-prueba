68eb2ce09a0fb9ec9933d22429b6f38c94a3b158
FernandoRiffo <fdo.riffo93@gmail.com>
2017-06-05 02:16:26 -0400

    cambios desde lenovo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel con Homestead
+                    Fernando Riffo
                 </div>
 
                 <div class="links">
